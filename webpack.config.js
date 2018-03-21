64c7e39706f35c18c522cea3d4e4ba913eabf456
kukabunga <troy_666@inbox.ru>
2018-03-21 23:22:18 +0300

    webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: {
+		main: "./src/app"
+	},
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: "[name].js"
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				commons: {
+					chunks: "initial",
+					/** Minimum number of times a module has to be duplicated until it's considered for splitting */
+					minChunks: 2,
+					/** Maximum number of initial chunks which are accepted for an entry point */
+					maxInitialRequests: 5,
+					/** Minimal size for the created chunk */
+					minSize: 0
+				},
+				vendor: {
+					test: /node_modules/,
+					chunks: "initial",
+					name: "vendor",
+					priority: 10,
+					enforce: true
+				}
+			}
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: "html-loader",
+						options: {minimize: false}
+					}
+				]
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+			filename: "./index.html"
+		})
+	]
+
+}
