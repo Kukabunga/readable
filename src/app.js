52752f8bda2f9bc6c7c61605209474b992423be3
kukabunga <troy_666@inbox.ru>
2018-04-04 23:46:53 +0300

    Add react-router-dom

--- src/app.js
+++ src/app.js
@@ -1,4 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import Loadable from 'react-loadable'
+import ProgressIndicator from './components/progress-indicator/ProgressIndicator'
 
-ReactDOM.render(<div>I'm dummy</div>, document.getElementById('app'));
+const MainPageLoadable = Loadable({
+    loader: () => import('./views/src/MainPage'),
+    loading: ProgressIndicator
+})
+
+const CategoriesPageLoadable = Loadable({
+    loader: () => import('./views/src/CategoriesViewPage'),
+    loading: ProgressIndicator
+})
+
+const PostsViewPageLoadable = Loadable({
+    loader: () => import('./views/src/PostsViewPage'),
+    loading: ProgressIndicator
+})
+
+const CreatePostPageLoadable = Loadable({
+    loader: () => import('./views/src/CreatePostPage'),
+    loading: ProgressIndicator
+})
+
+class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                {/* MainMenu */}
+                <Route exactPath ="/" render={() => ()}>
+                </Route>
+            </React.Fragment>
+        )
+    }
+}
